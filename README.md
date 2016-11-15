d1714c912676cdf480078c643f8ecbabbbd9fee5
ming.zhao <edwin404@163.com>
2016-11-15 10:01:21 +0800

    init

---
+++ README.md
@@ -0,0 +1,28 @@
+# 基于HTML的多级选择
+
+## 在线演示
+
+[http://edwin404.com/multi-selector/demo](http://edwin404.com/multi-selector/demo)
+
+## 如何使用
+
+step1. 引入js
+
+```html
+<script src="/path/to/js/multi-selector.js"></script>
+````
+
+step3. 使用JS代码初始化
+
+```javascript
+var demoStatic = new MultiSelector({
+    data: testData
+});
+```
+
+更多参照示例
+
+
+## 意见反馈
+
+[https://github.com/edwin404/multi-selector/issues](https://github.com/edwin404/multi-selector/issues)
---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp');
+var gulpPrint = require('gulp-print');
+var gulpUglify = require('gulp-uglify');
+var gulpCleanCss = require('gulp-clean-css');
+var gulpLess = require('gulp-less');
+
+gulp.task('buildJs', function () {
+    return gulp.src('src/**/*.js')
+        .pipe(gulpPrint(function (path) {
+            return "build: " + path;
+        }))
+        .pipe(gulpUglify())
+        .pipe(gulp.dest('dist'));
+});
+
+
+gulp.task('buildLess', function () {
+    return gulp.src('src/**/*.less')
+        .pipe(gulpPrint(function (path) {
+            return "build: " + path;
+        }))
+        .pipe(gulpLess())
+        .pipe(gulpCleanCss({keepSpecialComments: 0}))
+        .pipe(gulp.dest('dist'));
+});
+
+/*
+
+ /app
+ /app/src/
+ /app/dist/
+
+ */
+
+
+gulp.task('default', ['buildJs', 'buildLess'], function () {
+
+});
